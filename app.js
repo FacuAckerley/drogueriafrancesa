5a65205873f59eb6645701a187f8045622f1a8ce
Facu <facu.ackerley@gmail.com>
2020-11-30 12:49:25 -0300

    proyecto df

---
+++ app.js
@@ -0,0 +1,20 @@
+$('#myModal').on('shown.bs.modal', function () {
+    $('#myInput').trigger('focus')
+  })
+
+// Initialize and add the map
+function initMap() {
+  // The location of Uluru
+  const uluru = { lat: -34.77324, lng: -58.23165 };
+  // The map, centered at Uluru
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 17,
+    center: uluru,
+  });
+  // The marker, positioned at Uluru
+  const marker = new google.maps.Marker({
+    position: uluru,
+    map: map,
+  });
+}
+
